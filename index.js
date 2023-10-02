a327184423d75b5b9fbffba31d350ca379519670
mahdiosman2023 <mahdiosmanu78@gmail.com>
2023-10-02 23:31:32 +0300

    git@github.com:mahdiosman2023/phase-1-control-flow-lab.git

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someNumber){
   // Write your code here!
+ if (someNumber <= 400) {
+   return 'This one is on me!';
+ }
+ else if (someNumber > 2000 && someNumber < 2500) {
+  return 'I will gladly take your thirty bucks.';
+ }
+  else if (someNumber > 2500) {
+    return 'No can do.';
+  }
+
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city) {
   // Write your code here!
+  if (city === "NYC") {
+    return "Ok, sounds good.";
+   }
+else if (city === "Pittsburgh") {
+  return 'No go.';
+}
+}
+  
+function switchOnCharmFromTip(tip) {
+// Write your code here!
+if (tip === "generous") {
+  return 'Thank you so much.';
+}
+else if (tip === 'not as generous') {
+  return 'Thank you.';
+}
+else if (tip === 'thanks for everything') {
+  return 'Bye.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
 }
+
